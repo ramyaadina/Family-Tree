620b8b568b815db5296d49ce6e278c3fbc23db16
Ramya Adina <ramya_adina@yahoo.com>
2018-07-24 11:19:37 +0530

    capturing form params

--- client/src/Components/ChildForm.js
+++ client/src/Components/ChildForm.js
@@ -2,13 +2,41 @@ import React from 'react';
 import '../App.css'
 
 export default class ChildForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            name: '',
+            gender: 'm'
+        };
+
+        // this.handleNameChange = this.handleNameChange.bind(this);
+        this.handleGenderChange = this.handleGenderChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    // handleNameChange(event) {
+    //     this.setState({ name: event.target.value });
+    // }
+
+    handleGenderChange(event) {
+        this.setState({ gender: event.target.value });
+    }
+
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.name +this.state.gender);
+        event.preventDefault();
+    }
+
     render() {
         return (
             <div>
-                {this.props.value}
-                Name : <input type="text" /><br />
-                Gender : <input type="radio" name={this.props.value+"gender"} value="male" checked /> Male
-                <input type="radio" name={this.props.value+"gender"} value="female" /> Female
+                <form onSubmit={this.handleSubmit}>
+                    {this.props.value}
+                    <input type="text" onChange={this.props.nameChange} /><br />
+                    <input type="radio" name={this.props.value + "gender"} value="m" onChange={this.handleGenderChange} defaultChecked /> Male
+                    <input type="radio" name={this.props.value + "gender"} value="f" onChange={this.handleGenderChange} /> Female<br />
+                    <input type="submit" value="Submit" />
+                </form>
             </div>
         )
     }
